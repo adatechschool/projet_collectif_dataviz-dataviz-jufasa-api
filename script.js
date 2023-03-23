e415ca11743d0cdf69cf6733222d1ed074b277f6
Justine <justine-lambert@hotmail.fr>
2023-03-23 11:36:27 +0100

    Nouveau projet affichage de données sur les séismes

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+let affichage = document.getElementById("affichage");
+const urlBase = "https://earthquake.usgs.gov/fdsnws/event/1/"
 
-let timer = document.getElementById("timer")
-
-const displayTime = async () => {
-
-    let data = await fetch ("http://worldtimeapi.org/api/timezone/Europe/Paris")
+async function display(){
+    let data = await fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson") 
 
     let response = await data.json();
     console.log(response);
 
-    let values = response.datetime
-    console.log(values)
+    let seismes = response.features
+    console.log(`Dans l'heure précédente, il y a eu ${seismes.length} séismes.`)
+    seismes.forEach(seisme => {
+        let id = seisme.id;
+        let place = seisme.properties.place;
+        let mag = seisme.properties.mag;
+        let date = new Date(seisme.properties.time);
+        let dateStr = dateToString(date);
 
-    timer.innerHTML = values
+        console.log(`(id=${id}) Un séisme à ${place}, le ${dateStr}, de magnitude ${mag}`)
 
-    
-
+    });
 }
 
-setInterval(displayTime, 1000)
+function dateToString(date) {
+    let day = date.getDate();
+    let month = date.getMonth()+1;
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+
+    return `${day}/${month}/${year} à ${hour}h${minute}`
+}
+display()
